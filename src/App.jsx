61f073c6928bf0149968284d2fb13731b66a41c5
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2022-12-20 11:39:01 +0100

    add container styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { useGetListItemsQuery } from "./api/shopping-list-api/shopping-list-api";
+import classes from "./App.module.css";
 
 function App() {
   const { data = [] } = useGetListItemsQuery();
-
   console.log(data); // do usunięcia
 
   return (
-    <div>
-      <div>Shopping List</div>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item.productName}</li>
-        ))}
-      </ul>
-    </div>
+    <main className={classes.container}>
+      <div>Shopping list</div>
+      <div>
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>{item.productName}</li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
 

e74c294b520f8224f7a7c5b4496c142d7d924a35
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2022-12-23 11:17:48 +0100

    add List component styles

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,7 @@
 import { RiEdit2Fill } from "react-icons/ri";
 import { IoTrashBinSharp } from "react-icons/io5";
+import classes from "./List.module.css";
+
 
 const List = ({ listItems }) => {
   return (
@@ -7,14 +9,14 @@ const List = ({ listItems }) => {
       {listItems.map((item) => {
         const { id, productName } = item;
         return (
-          <article key={id}>
-            <p>{productName}</p>
+          <article key={id} className={classes["shopping-item"]}>
+            <p className={classes["product-name"]}>{productName}</p>
             <div>
-              <button type="button">
-                <RiEdit2Fill />
+              <button type="button" className={classes["edit-btn"]}>
+                <RiEdit2Fill className={classes.icon}/>
               </button>
-              <button type="button">
-                <IoTrashBinSharp />
+              <button type="button" className={classes["delete-btn"]}>
+                <IoTrashBinSharp className={classes.icon}/>
               </button>
             </div>
           </article>
